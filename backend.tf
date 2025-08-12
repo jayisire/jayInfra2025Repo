b1e41d5365475a33feb12dc7f83cac1daeebaf53
jayisire <96596503+jayisire@users.noreply.github.com>
2025-08-12 16:40:49 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "jay-dev-tf-state-bucket-2025"
     key = "main"
-    region = "us-east-1"
+    region = "us-east-2"
     dynamodb_table = "jay-dynamodb-table-2025"
   }
 }
